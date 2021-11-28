66209ef0fe4c70e85ca4ad52977a010701a0bdf1
reyhannaufal <reyhannaufal21@gmail.com>
2021-11-28 14:16:01 +0700

    feat: test loader function

--- src/app.js
+++ src/app.js
@@ -183,6 +183,39 @@ document.addEventListener("keyup", (e) => {
 /**
  * Load model
  */
+const manager = new THREE.LoadingManager();
+manager.onStart = function (url, itemsLoaded, itemsTotal) {
+  console.log(
+    "Started loading file: " +
+      url +
+      ".\nLoaded " +
+      itemsLoaded +
+      " of " +
+      itemsTotal +
+      " files."
+  );
+};
+
+manager.onLoad = function () {
+  console.log("Loading complete!");
+};
+
+manager.onProgress = function (url, itemsLoaded, itemsTotal) {
+  console.log(
+    "Loading file: " +
+      url +
+      ".\nLoaded " +
+      itemsLoaded +
+      " of " +
+      itemsTotal +
+      " files."
+  );
+};
+
+manager.onError = function (url) {
+  console.log("There was an error loading " + url);
+};
+
 const loader = new GLTFLoader();
 let ghostGroups = [];
 loader.load(
