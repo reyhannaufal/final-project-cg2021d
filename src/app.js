995b0e5c328393bea4999fa5b255763b8ad2009e
reyhannaufal <reyhannaufal21@gmail.com>
2021-12-03 17:02:51 +0700

    feat: update some comment

--- src/app.js
+++ src/app.js
@@ -341,8 +341,8 @@ fbxLoader.load("mremireh_o_desbiens.fbx", (fbx) => {
 
 /**
  * DO NOT TOUCH THIS
- * UNCOMMENT TO TEST
- * MAKE SURE TO COMMENT OUT BEFORE PUSHING
+ * COMMENT TO TEST
+ * MAKE SURE TO UNCOMMENT OUT BEFORE PUSHING
  */
 window.addEventListener("start", () => {
   overlay.style.display = "block";
